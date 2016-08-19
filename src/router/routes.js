3797b3805716e9543e193a4b700c92efe0d92291
Emma J <codemzy@gmail.com>
2016-08-19 09:44:06 +0000

    Updated routes to wrap Resources with the higher order component

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
+// higher order component
+import requireAuth from '../components/Require_Auth'
+
 // components
 import App from '../components/App';
 import Resources from '../components/Resources';
 
 export default (
     <Route path='/' component={App}>
-        <Route path="/resources" component={Resources} />
+        <Route path="/resources" component={requireAuth(Resources)} />
     </Route>
 );
