0fdf1ce911e852f9a5d1720d6ff2f1a08e5f71e0
Emma J <codemzy@gmail.com>
2016-08-19 11:26:43 +0000

    Added context from router to HOC to push user back to home route

--- src/components/Require_Auth.js
+++ src/components/Require_Auth.js
@@ -3,10 +3,21 @@ import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
     class Authentication extends React.Component {
+        constructor(props, context) {
+            super(props);
+            context.router;
+        }
+        componentWillMount() {
+            this.context.router.push('/');
+        }
         render() {
             return <ComposedComponent {...this.props} />;
         }
     }
+    
+    Authentication.contextTypes = {
+        router: React.PropTypes.object.isRequired
+    };
     
     return connect((state) => {
     return {
